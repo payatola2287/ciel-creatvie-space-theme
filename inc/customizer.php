63d04b74f1a11a2609039bce4a8371ebe2b3511d
Paolo Gallardo <111101322+consideredpaolo@users.noreply.github.com>
2023-10-31 07:46:14 +0800

    pull minified asset instead

--- inc/customizer.php
+++ inc/customizer.php
@@ -56,6 +56,6 @@ function ciel_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function ciel_customize_preview_js() {
-	wp_enqueue_script( 'ciel-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'ciel-customizer', get_template_directory_uri() . '/js/customizer.min.js', array( 'customize-preview' ), _S_VERSION, true );
 }
 add_action( 'customize_preview_init', 'ciel_customize_preview_js' );
