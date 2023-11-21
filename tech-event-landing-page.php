1e80594b1efd29f1d4f53a43c8c4c746d432f6c9
Paolo Gallardo <paolo.j.gallardo@gmail.com>
2023-11-22 06:55:43 +0800

    landing page tempalte

---
+++ tech-event-landing-page.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Tech Event Landing Page
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+	<?php get_template_part( 'template-parts/content', 'banner' ); ?>
+	<?php
+if( have_rows('flexible_content') ):
+	
+	while ( have_rows('flexible_content') ) : the_row(); 
+	set_query_var( 'section_id', get_row_index() );
+
+	?>
+
+		<div class="section section_<?php echo get_row_index(); ?> " id="<?php echo get_row_layout(); ?>_<?php echo get_row_index(); ?>">
+			<?php get_template_part('flexible/content', get_row_layout()); ?>
+		</div>
+
+		<?php 
+	endwhile;
+
+else:
+
+	if ( have_posts() ) {
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+		}
+	}
+endif;
+	?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
