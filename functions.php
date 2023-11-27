ad50f0f1680baf3cc591fd842ced4156f9d9a2b4
Paolo Gallardo <paolo.j.gallardo@gmail.com>
2023-11-28 07:19:59 +0800

    versoini update

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.2.7' );
+	define( '_S_VERSION', '1.2.8' );
 }
 
 if ( ! function_exists( 'ciel_setup' ) ) :
